dcca0f8917fb1da1986c446ee7a286d558e22975
Sinar Dyas <sinardyas@Sinars-MacBook-Pro.local>
2019-06-17 18:57:52 +0700

    add args for puppeteer

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -3,7 +3,10 @@ const puppeteer = require('puppeteer');
 const urlBuilder = (user) => `https://www.last.fm/user/${user}/library/artists?date_preset=LAST_7_DAYS`;
 
 module.exports = async (user) => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args:['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 926 });
     await page.goto(urlBuilder(user));
